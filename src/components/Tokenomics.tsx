f9ae7315ecd9b7cfbdb43cd03d65b61c33803ef9
Nicolas Quintana <nquintan@uwaterloo.ca>
2022-05-18 13:55:44 -0400

    Finalized pie chart

--- src/components/Tokenomics.tsx
+++ src/components/Tokenomics.tsx
@@ -9,7 +9,20 @@ const Tokenomics: React.FC = () => {
       <h1>Tokenomics</h1>
       <h2>Token Distribution</h2>
 
-      <Pie data={data} />
+      <Pie
+      options={{
+        responsive: true,
+        plugins: {
+          tooltip: {
+            callbacks: {
+              footer: footer,
+            }
+          }
+        }
+      }}
+      data={data} 
+      className="pie-chart"/>
+
       <div className="table-container">
         <table>
           <thead>
@@ -153,6 +166,64 @@ const Tokenomics: React.FC = () => {
     </>
   );
 };
+
+const footer = (tooltipItems: any) => {
+  const index = tooltipItems[0].dataIndex;
+  
+  return (
+    "Allocation: " + tooltipData[index].Allocation
+    + "\nUnlock Schedule: " + tooltipData[index].Unlock
+    + "\nUse Case: " + tooltipData[index].Use
+    );
+};
+
+const tooltipData = [
+  {
+    Allocation: '150,000,000,000',
+    Unlock: 'Locked at launch for a minimum of 2 years. May be relocked after a discussion with the Ascension community.',
+    Use: 'Ascension tokens will be used to pay core team members for their work on the project.',
+  },
+  {
+    Allocation: '225,000,000,000',
+    Unlock: 'Locked at launch. Issuance will start in Q4 of 2022.',
+    Use: 'P2E tokens will be used to reward players for their in-game actions.',
+  },
+  {
+    Allocation: '225,000,000,000',
+    Unlock: 'Locked at launch. Issuance will start in Q3 of 2022.',
+    Use: 'Staking Reward tokens will be used to reward players for staking their tokens to increase the locked liquidity in the project.',
+  },
+  {
+    Allocation: '20,000,000,000',
+    Unlock: 'Linear release from the time of the IDO.',
+    Use: 'Air Drop tokens will be used for community marketing initiatives.',
+  },
+  {
+    Allocation: '50,000,000,000',
+    Unlock: 'Locked until the strategic sales have officially been completed. From that point in time, locked for 6 months, then released via a 6-month linear release.',
+    Use: 'Strategic Sale tokens will be saved for sales and partnerships in the project\'s future.',
+  },
+  {
+    Allocation: '65,000,000,000',
+    Unlock: 'Not locked, available for usage at the discretion of the team.',
+    Use: 'Marketing tokens will be used for marketing in the crypto space (calls, promotions, etc.). In the later stages of game development, control of this wallet will be transferred to the $ASCN token holder DAO.',
+  },
+  {
+    Allocation: '110,000,000,000',
+    Unlock: 'Locked in the liquidity pool for a minimum of 1 year. May be relocked after a discussed with the Ascension team and the Ascension community.',
+    Use: 'IDO tokens are the tokens that will be released for the initial public sale.',
+  },
+  {
+    Allocation: '55,000,000,000',
+    Unlock: 'Locked until the private raise has been officially completed. From that point in time, locked for 6 months, the released via a 6-month linear release.',
+    Use: 'Private Raise tokens will be saved for official fundraising and sales to angel investors.',
+  },
+  {
+    Allocation: '100,000,000,000',
+    Unlock: 'Linear release from the time of the IDO.',
+    Use: 'Game Operations & Development tokens will be used for the further development of the Ascension Ecoverse game and all future Ascension initiatives. In the later stages of game development, control of this wallet will be transferred to the $ASCN token holder DAO',
+  },
+];
 
 const data = {
   labels: ['Ascension Tokens', 'P2E Rewards', 'Staking Rewards', 'Air Drops', 'Strategic Sales', 'Marketing', 'IDO', 'Private Raise', 'Game Operations & Development'],
