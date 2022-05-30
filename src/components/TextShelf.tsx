f7e066fddf05557d9ebd22571a1f52d9c5a87f4e
Nicolas Quintana <nquintan@uwaterloo.ca>
2022-05-30 10:00:27 -0400

    Added team headshots

--- src/components/TextShelf.tsx
+++ src/components/TextShelf.tsx
@@ -16,16 +16,17 @@ const List: React.FC<ListProps> = ({ desc }) => {
 }
 
 type TextShelfProps = {
-    media: { name: string; key: number; desc: string | String[]; }[];
+    media: { name: string; key: number; desc: string | String[]; headshot?: string}[];
   };
 
 const TextShelf: React.FC<TextShelfProps> = ({ media }) => {
     return (
       <div className="media-shelf-container">
         <ul className="media-shelf">
-          {media.map(({ name, key, desc }) => (
+          {media.map(({ name, key, desc, headshot }) => (
             <li key={key} className="media-shelf--item">
               <figure>
+                {headshot && <img src={headshot} alt="headshot" />}
                 <h2>{name}</h2>
                 {typeof desc === "string" ? <p>{desc}</p> : <List desc={desc} /> }
               </figure>
