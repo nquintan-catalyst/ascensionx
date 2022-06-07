543660dab8cfc8f487d15f2dbc455808aac312f9
Nicolas Quintana <nquintana@catalystreaction.com>
2022-06-07 11:54:24 -0400

    Fixed tokenoimcs chart legend

--- src/components/Tokenomics.tsx
+++ src/components/Tokenomics.tsx
@@ -32,6 +32,7 @@ const Tokenomics: React.FC = () => {
                 font: {
                   size: 17,
                 },
+                color: 'white',
               },
             },
           },
